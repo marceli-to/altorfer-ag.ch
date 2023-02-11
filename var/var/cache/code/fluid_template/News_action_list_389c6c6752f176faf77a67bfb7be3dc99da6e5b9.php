329e8f5b3a522051b7f564d13019202d104d70c9
Marcel Stadelmann <m@marceli.to>
2023-02-11 15:32:43 +0100

    composer

--- var/var/cache/code/fluid_template/News_action_list_389c6c6752f176faf77a67bfb7be3dc99da6e5b9.php
+++
@@ -1,6 +0,0 @@
-<?php
-class News_action_list_389c6c6752f176faf77a67bfb7be3dc99da6e5b9 extends \TYPO3Fluid\Fluid\Core\Compiler\AbstractCompiledTemplate
- implements \TYPO3Fluid\Fluid\Core\Compiler\UncompilableTemplateInterface
-{
-}
-#
